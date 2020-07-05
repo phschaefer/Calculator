5afb4fc1851f746a9b2b81d3de1e9649f257c7b3
Philipp <ph.schaefer@protonmail.com>
2020-07-05 16:53:58 +0200

    Initial commit

---
+++ Array/src/main.kts
@@ -0,0 +1,14 @@
+import kotlin.math.PI
+import kotlin.script.templates.standard.ScriptTemplateWithArgs
+
+class Circle(radius: Double){
+    var radius: Double = radius
+
+    fun area() :Double = radius * radius * PI
+}
+
+fun main(){
+    //val c:Circle = Circle(1.0)
+
+    println("Trest")
+}
---
+++ Test/src/app.kt
@@ -0,0 +1,6 @@
+data class Test (var name:String)
+
+fun main () {
+    val test:Test = Test("Yeah ich bin ein Test")
+    println(test.name)
+}
